4bc2601346897d772997ae2dd50dd1cd63486960
James Middleton <james@midz.dev>
2024-01-12 19:57:51 +0000

    test getSetCookie

--- routes/index.ts
+++ routes/index.ts
@@ -1,7 +1,13 @@
-export default eventHandler(async () => {
-  const loginPage = fetch('https://www.sykescottages.co.uk/account/login')
+// const getCookie = (name: string, headers: Headers) => {
 
-  return loginPage.then(x => [...x.headers.entries()]);
+// }
+
+export default eventHandler(async () => {
+  const loginPageResp = await fetch('https://www.sykescottages.co.uk/account/login')
+
+  // const phpSessId = [...loginPageResp.headers.entries()].find(([k, v]) => k === 'set-cookie' && v.startsWith('PHPSESSID='))[1].split(';')[0].split('=')[1];
+
+  return loginPageResp.headers.getSetCookie();
 
   // const sessionId = '529utnjpjk9t3pdv30csns3qvb';
   // const propertyId = '21953';
